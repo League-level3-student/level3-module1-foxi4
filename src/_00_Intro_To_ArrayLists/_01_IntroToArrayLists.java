157d520091f2c5c79677dd9d9c3d5b30c9133cbc
laurenkan. <laurenkan.@localhost>
2023-01-15 15:16:44 -0800

    1/15/23

--- src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
+++ src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
@@ -1,21 +1,68 @@
 package _00_Intro_To_ArrayLists;
 
+import java.util.ArrayList;
+
 public class _01_IntroToArrayLists {
     public static void main(String[] args) {
         // 1. Create an array list of Strings
         //    Don't forget to import the ArrayList class
+    	ArrayList<String> strings = new ArrayList<String>();
 
         // 2. Add five Strings to your list
-
+strings.add("1");
+strings.add("2");
+strings.add("3");
+strings.add("4");
+strings.add("5");
         // 3. Print all the Strings using a standard for-loop
-
+for(int i=0; i<strings.size(); i++) {
+	System.out.println(strings.get(i));
+}
+System.out.println(" ");
         // 4. Print all the Strings using a for-each loop
-
+for(String i : strings) {
+	System.out.println(i);
+}
+System.out.println(" ");
         // 5. Print only the even numbered elements in the list.
-
+for(int i=0; i<strings.size(); i++) {
+	
+	if(i%2==0) {
+		//i are the positions
+		System.out.println(i);
+	}
+}
+System.out.println(" ");
         // 6. Print all the Strings in reverse order.
 
+	String max = strings.get(strings.size()-1);
+	String min = strings.get(0);
+	String minONE = strings.get(1);
+	String minTWO = strings.get(2);
+	String minTHREE = strings.get(3);
+	/* if(max == strings.get(i)) {
+		System.out.println(max);
+	}
+	if(min == strings.get(i)) {
+		System.out.println(min);
+	 } */
+	System.out.println(max);
+	System.out.println(minTHREE);
+	System.out.println(minTWO);
+	System.out.println(minONE);
+	System.out.println(min);
+	//lol i did it the wrong way BUT IT WORSK OKAY
+System.out.println(" ");
         // 7. Print only the Strings that have the letter 'e' in them.
-        
+for(int i=0; i<strings.size(); i++) {
+	if(strings.get(i)=="e") {
+		System.out.println(i);
+	}
+	else {
+		System.out.println("Arraylist element "+i+": does not have the letter 'e' in them.");
+	}
+	
+	}
+
     }
 }
--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,6 +1,14 @@
 package _00_Intro_To_ArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
     /*
      * Create a GUI with two buttons. One button reads "Add Name" and the other
      * button reads "View Names". When the add name button is clicked, display
@@ -14,4 +22,87 @@ public class _02_GuestBook {
      * Guest #4: Donny Doners
      */
 
+	public class _02_GuestBook extends JFrame implements ActionListener {
+	    JButton addNames;
+	    JButton viewNames;
+	    ArrayList<String> names = new ArrayList<String>();
+
+	    public _02_GuestBook() {
+	        start();
+	    }
+
+	    public void start() {
+	        JFrame frame = new JFrame("Guest Book!!!!!");
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setSize(500, 500);
+	        JPanel panel = new JPanel();
+	        frame.add(panel);
+	        addNames = new JButton("Add Name");
+	        viewNames = new JButton("View Names");
+	        panel.add(addNames);
+	        panel.add(viewNames);
+	        addNames.addActionListener(this);
+	        viewNames.addActionListener(this);
+	        frame.setVisible(true);
+	    }
+
+	    public static void main(String[] args) {
+	        _02_GuestBook guestbook = new _02_GuestBook();
+	    }
+
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+	        if (e.getSource() == addNames) {
+	            String name = JOptionPane.showInputDialog("Enter a name: ");
+	            names.add(name);
+	        }
+
+	        if (e.getSource() == viewNames) {
+	            for (int i = 0; i < names.size(); i++) {
+	                System.out.println("Guest #" + i + ": " + names.get(i));
+	            }
+	        }
+	    }
+	}
+	/* INCORRECT CODE BELOW
+	 
+	public GuestBook() {
+		start();
+	}
+	public void start(){
+		//add panel/frame/actionlistener
+	JFrame frame = new JFrame("Guest Book!!!!!");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(500,500);
+    JPanel panel = new JPanel();
+    frame.add(panel);
+    panel.add(addNames);
+    panel.add(viewNames);
+	addNames = new JButton("Add Name");
+	viewNames = new JButton("View Names");
+	addNames.addActionListener(this);
+	viewNames.addActionListener(this);
+	frame.setVisible(true);
+	}
+	public static void main(String[] args) {
+		GuestBook guestbook = new GuestBook();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+				
+		if(addNames == e.getSource()) {
+			//line of code checks (e.getSource) what was "triggered," in this case it's addNames
+			String name = JOptionPane.showInputDialog("Enter a name: ");
+			names.add(name);
+		}
+		
+		if(viewNames == e.getSource()) {
+			for(int i=0; i<names.size(); i++) {
+				System.out.print("Guest #" +i+": " +names.get(i));
+			}
+		}
+	}
+
 }
+*/
